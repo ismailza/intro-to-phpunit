9e7f3b3b636c4b6520748b808b5698ebc709d3d9
Ismail ZAHIR <ismailza407@gmail.com>
2024-06-03 09:06:42 +0100

    add scope recent to product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
 
 class Product extends Model
 {
@@ -16,4 +17,14 @@ class Product extends Model
         'stock',
         'image',
     ];
+
+    /**
+     * Scope a query to order products by the most recent.
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeRecent(Builder $query): Builder
+    {
+        return $query->orderBy('created_at', 'desc');
+    }
 }
